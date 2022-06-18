3df195667b72c375f9cdaea2ec01bf4dbead0bc9
Mangesh2042 <mangesh2042@gmail.com>
2022-06-18 14:47:59 +0530

    completed

--- src/components/Productdetails.jsx
+++ src/components/Productdetails.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 const Productdetails = () => {
+const {id}=useParams()
+console.log(id);
+
+ const [data,setData]=useState([])
+
+
+useEffect(()=>{
+  fetch(`http://localhost:3004/products/${id}`).then((res)=>res.json()).then((res)=>{
+    setData(res)
+  })
+},[])
+
+
+
   return (
-    <div>Productdetails</div>
+    <>
+        <div>{data.id+1}</div>
+        <div>{data.name}</div>
+        <div>{data.price}</div>
+    </>
+   
   )
 }
 
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react'
+
+import { Link } from "react-router-dom";
+
 const Products = () => {
   const [data,setData]=useState([])
   useEffect(()=>{
@@ -8,12 +11,34 @@ const Products = () => {
   },[])
 
 
- 
 
+return(<>
 
+  <table>
+    <thead>
+      <td>id</td>
+      <td>Name</td>
+      <td>more Details</td>
+    </thead>
+    <tbody>
 
-  
-  
+      {data.map((ele)=>{
+        return(<>
+          <tr>
+            <td>{ele.id+1}</td>
+            <td>{ele.name}</td>
+            <td><Link to={`/products/${ele.id}`}>more</Link>
+          </td>
+          </tr>
+        </>)
+      })}
+
+       
+    </tbody>
+  </table>
+
+</>)
+
 }
 
 export default Products
